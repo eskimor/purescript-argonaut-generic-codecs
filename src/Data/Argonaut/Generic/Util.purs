182777bfb6a316664089301627ecb6f33d564300
Robert Klotzner <robert.klotzner@gmx.at>
2016-09-24 19:36:55 +0200

    Implemented IsRecord utility functions

--- src/Data/Argonaut/Generic/Util.purs
+++ src/Data/Argonaut/Generic/Util.purs
@@ -1,12 +1,11 @@
 module Data.Argonaut.Generic.Util where
 
 import Prelude
-
+import Data.Array (head, null, length)
 import Data.Foldable (all)
+import Data.Generic (GenericSignature(SigRecord), GenericSpine(SRecord), DataConstructor)
+import Data.Maybe (fromMaybe, Maybe(..))
 import Data.String (lastIndexOf, drop)
-import Data.Array (null, length)
-import Data.Generic (DataConstructor)
-import Data.Maybe (Maybe(..))
 
 allConstructorsNullary :: Array DataConstructor -> Boolean
 allConstructorsNullary = all (null <<< _.sigValues)
@@ -15,9 +14,23 @@ allConstructorsNullary = all (null <<< _.sigValues)
 -- | Needed for applying unwrapUnaryRecords.
 isUnaryRecord :: Array DataConstructor -> Boolean
 isUnaryRecord constrSigns = length constrSigns == 1 -- Only one constructor
-                            
+
 
 stripModulePath :: String -> String
 stripModulePath constr = case lastIndexOf "." constr of
                     Nothing -> constr
                     Just i -> drop (i+1) constr
+
+-- | Check whether a given spine is a record
+spineIsRecord :: GenericSpine -> Boolean
+spineIsRecord r = case r of
+  SRecord _ -> true
+  _ -> false
+
+-- | Check whether a constructor is a Haskell constructor with record fields.
+constructorIsRecord :: DataConstructor -> Boolean
+constructorIsRecord constr = fromMaybe false $ do
+  record <- (_ $ unit) <$> head constr.sigValues
+  case record of
+    SigRecord _ -> pure true
+    _ -> pure false
